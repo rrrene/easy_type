fa3714a541c5a02bbb399e331a44a8f89512e9c0
Bert Hajee <hajee@moretIA.com>
2014-02-01 15:58:58 +0100

    Added some documentation
    Signed-off-by: Bert Hajee <hajee@moretIA.com>

--- lib/easy_type/command_builder.rb
+++ lib/easy_type/command_builder.rb
@@ -1,8 +1,24 @@
 module EasyType
+	#
+	# The command builder is a class that helps building an OS command sequence. You can specify 
+	# a base command on creation. With the << you can add parameters and options in the order you would 
+	# like. Sometimes you need to execute commands before and/or after the base command. The CommandBuilder
+	# supports this by using the `before` and `after` methods
+	#
 	class CommandBuilder
 		attr_accessor :command, :line
 		attr_reader :before_results, :after_results, :options
 
+		#
+		# Initializes a new command builder.
+		#
+		# @param [Method] context the context into which the command is called. The context defines what 
+		#                 properties and methods are available.
+		# @param command  the de base command
+		# @param line
+		# @param options
+		# @return [CommandBuilder] command_builder
+		#
 		def initialize(context, command, line = '', options = {})
 			@context = context
 			@command = translate_to_method(command)
@@ -14,11 +30,18 @@ module EasyType
 			@options = options
 		end
 
+		#
+		# @param line the part that is added to the base command
+		# @return [CommandBuilder] command_builder
+		#
 		def <<(line)
 			@line << ' ' << line
 			self
 		end
 
+		#
+		# @param command The command to be geving before the main command
+		#
 		def before(command = nil)
 			if command
 				@before << command
@@ -28,6 +51,9 @@ module EasyType
 			end
 		end
 
+		#
+		# @param command The command to be geving after the main command
+		#
 		def after(command = nil)
 			if command
 				@after << command
@@ -37,6 +63,10 @@ module EasyType
 			end
 		end
 
+		#
+		# Execute all the commands given. First the before commands, then the main command, and last the
+		# after commands.
+		#
 		def execute
 			@before.each do | before|
 				@before_results << @command.call(before, @options)
@@ -50,6 +80,7 @@ module EasyType
 
 		private
 
+		# @private
 		def translate_to_method(method_identifier)
 			case  method_identifier.class.to_s
 				when 'Symbol', 'String'
@@ -61,6 +92,7 @@ module EasyType
 			end
 		end
 
+		# @private
 		def valid_method_for(method_identifier)
 			method_identifier = method_identifier.to_s if RUBY_VERSION == "1.8.7"
 			unless @context.methods.include?(method_identifier) 
